8456057c572094139ae248a523dd8becc05a02af
Thomas Pinella <txpinella@gmail.com>
2015-09-19 10:25:01 -0400

    latest

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', './views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home', {
+	title: 'Welcome'
+	});
+});
+
+var server = app.listen(app.get('port'), function () {
+	console.log('Node app is running on port: ' + app.get('port'));
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log('connected!');
+    socket.on('chat message', function(msg) {
+        console.log('message' + msg);
+        io.emit('chat message', msg);
+    });
+});
